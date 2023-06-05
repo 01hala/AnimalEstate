ac37eeabf7e0dab7c7c388fdd4ebec72524e6a0c
ucat <ucat.games@gmail.com>
2023-06-05 20:23:27 +0800

    rank

--- start_node.bat
+++ start_node.bat
@@ -9,6 +9,7 @@ sleep 2
 
 start ./bin/debug/login.exe ./config/config_node.txt login
 start ./bin/debug/player.exe ./config/config_node.txt player
+start ./bin/debug/rank.exe ./config/config_node.txt rank
 start ./bin/debug/match.exe ./config/config_node.txt match
 start ./bin/debug/room.exe ./config/config_node.txt room
 start ./bin/debug/game.exe ./config/config_node.txt game
